a63102b6becf6f8e1406bd66ae7f9423669fecf6
Maede-alv <maede.alavinezhad@gmail.com>
2025-08-24 10:31:19 +0330

    feat: add new service pipeline

--- jobs/services.groovy
+++ jobs/services.groovy
@@ -17,3 +17,23 @@ pipelineJob('service-A') {
         scm('H/2 * * * *')
     }
 }
+
+pipelineJob('service-B') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Maede-alv/jenkins-pr-1.git')
+                        credentials('github-token')
+                    }
+                    branch('*/master')
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+    triggers {
+        scm('H/2 * * * *')
+    }
+}
